e4cb391a78463c9aa1d7b8408f7a86cebba1a087
lecepin <383810086@qq.com>
2022-03-31 17:07:06 +0800

    feat: action script

--- scripts/update-url-doc.js
+++ scripts/update-url-doc.js
@@ -1,11 +1,26 @@
 const glob = require("glob");
+const fs = require("fs");
+const path = require("path");
 
 glob(
   "images/**",
   {
     nodir: true,
   },
-  function (er, files) {
-    console.log(files);
+  (er, files) => {
+    let content = "# URL\n";
+
+    content += "\n ## From Github";
+
+    files.map((item) => {
+      content += `\n - [https://raw.githubusercontent.com/lecepin/picture-host/main/${item}](https://raw.githubusercontent.com/lecepin/picture-host/main/${item})`;
+    });
+
+    content += "\n ## From JsDelivr";
+    files.map((item) => {
+      content += `\n - [https://cdn.jsdelivr.net/gh/lecepin/picture-host/${item}](https://cdn.jsdelivr.net/gh/lecepin/picture-host/${item})`;
+    });
+
+    fs.writeFileSync(path.resolve(__dirname, "../URL.md"), content);
   }
 );
